a075e090c023940272c9ecdd4cbe8249314382c6
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-23 03:47:44 -0400

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Lori Rothermel on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
